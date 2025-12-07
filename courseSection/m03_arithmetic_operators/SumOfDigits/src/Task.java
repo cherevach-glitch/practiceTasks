12ac2896e39e672d44b2fce0cbf4149ed9231225
cherevach-glitch <cherevachandrei@gmail.com>
2025-12-08 01:05:55 +0200

    completed SumOfDigits

--- courseSection/m03_arithmetic_operators/SumOfDigits/src/Task.java
+++ courseSection/m03_arithmetic_operators/SumOfDigits/src/Task.java
@@ -1,12 +1,17 @@
 public class Task {
     public static void main(String[] args) {
-        int number = 123;
 
-        // ----Do not change below lines. needed for testing---
-        number = args.length > 0 ? Integer.parseInt(args[0]) : number;
-        // ----------------------------------------------
+        int number = 256;
+        int digit1, digit2, digit3;
+        int sum;
 
-        //Write your solution here
+        digit1 = number / 100;
+        digit2 = (number / 10) % 10;
+        digit3 = number % 10;
 
+        sum = digit1 + digit2 + digit3;
+
+        System.out.println("Input: number = " + number);
+        System.out.println("Output: Sum of the digits: " + sum);
     }
 }
