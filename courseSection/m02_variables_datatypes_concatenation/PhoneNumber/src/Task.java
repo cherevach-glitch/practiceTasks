3a204dd9830e81a095d409cff8a9ba3378b1f199
cherevach-glitch <cherevachandrei@gmail.com>
2025-12-08 00:38:22 +0200

    completed PhoneNumber task

--- courseSection/m02_variables_datatypes_concatenation/PhoneNumber/src/Task.java
+++ courseSection/m02_variables_datatypes_concatenation/PhoneNumber/src/Task.java
@@ -1,7 +1,11 @@
 public class Task {
     public static void main(String[] args) {
 
-        //Write your solution here
+        int areaCode = 303;
+        int localNumber = 5552345;
 
+        String phoneNumber = "(" + areaCode + ")-" + localNumber;
+
+        System.out.println("Calling number " + phoneNumber);
     }
 }
