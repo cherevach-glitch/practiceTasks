ffce7d0e8fbec094ba359f8e87e3ca4be4400b7e
cherevach-glitch <cherevachandrei@gmail.com>
2025-12-08 00:46:26 +0200

    completed Primitives task

--- courseSection/m02_variables_datatypes_concatenation/ProductInfo/src/Task.java
+++ courseSection/m02_variables_datatypes_concatenation/ProductInfo/src/Task.java
@@ -1,7 +1,17 @@
 public class Task {
     public static void main(String[] args) {
 
-        //Write your solution here
+        String name = "Blink Video Doorbell";
+        double price = 129.98;
+        int numberOfReviews = 115116;
+        float rating = 4.3f;
+        boolean onSale = true;
 
+        System.out.println("----- PRODUCT INFORMATION -----");
+        System.out.println(name);
+        System.out.println("$" + price);
+        System.out.println(numberOfReviews + " ratings");
+        System.out.println(rating + " * * * *");
+        System.out.println("onSale = " + onSale);
     }
 }
