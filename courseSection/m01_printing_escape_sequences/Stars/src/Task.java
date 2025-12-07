90e7b5630ab80952b5304d85536c52fd72bb92ae
cherevach-glitch <cherevachandrei@gmail.com>
2025-12-07 19:27:44 +0200

    completed PrintPaths task

--- courseSection/m01_printing_escape_sequences/Stars/src/Task.java
+++ courseSection/m01_printing_escape_sequences/Stars/src/Task.java
@@ -1,15 +1,9 @@
 public class Task {
     public static void main(String[] args) {
 
-        for (int i = 1; i <= 3; i++) {
-            for (int j = 1; j <= 3 - i; j++) {
-                System.out.print(" ");
-            }
-            for (int j = 1; j <= 2 * i - 1; j++) {
-                System.out.print("*");
-            }
-            System.out.println();
-        }
+        System.out.println("*");
+        System.out.println("***");
+        System.out.println("*****");
 
     }
 }
