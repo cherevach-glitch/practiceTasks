9ba4b4cf0fe08b4630646a5252e4175aa3b872d9
cherevach-glitch <cherevachandrei@gmail.com>
2025-12-08 01:17:22 +0200

    completed increseCount

--- courseSection/m04_unary_shorthand_operators/IncreaseCount/src/Task.java
+++ courseSection/m04_unary_shorthand_operators/IncreaseCount/src/Task.java
@@ -2,10 +2,11 @@ public class Task {
     public static void main(String[] args) {
 
         int count = 5;
-        //Do not change below line:needed for testing---
-        count = args.length > 0 ? Integer.parseInt(args[0]) : count;
 
-        // Write your solution here
+        count = count + 1;
+        count++;
+        count += 1;
 
+        System.out.println("count = " + count);
     }
 }
