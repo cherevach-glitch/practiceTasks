a18238e911c286b9441960cb53d5c23126cca97d
cherevach-glitch <cherevachandrei@gmail.com>
2025-12-08 01:08:40 +0200

    completed SwapValues

--- courseSection/m03_arithmetic_operators/SwapValues/src/Task.java
+++ courseSection/m03_arithmetic_operators/SwapValues/src/Task.java
@@ -4,15 +4,14 @@ public class Task {
         int num1 = 10;
         int num2 = 20;
 
-        // ----Do not change below lines. needed for testing---
-        num1 = args.length > 0 ? Integer.parseInt(args[0]) : num1;
-        num2 = args.length > 0 ? Integer.parseInt(args[1]) : num2;
-        // ------------------------------
-
-        //Write your solution here
+        int temp = num1;
+        num1 = num2;
+        num2 = temp;
 
         System.out.println("num1 = " + num1);
         System.out.println("num2 = " + num2);
-
-     }
+    }
 }
+
+
+
