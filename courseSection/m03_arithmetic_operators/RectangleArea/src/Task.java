a96fba39760e48691037de5d795f660e3c16e877
cherevach-glitch <cherevachandrei@gmail.com>
2025-12-08 01:01:22 +0200

    completed RectangleArea

--- courseSection/m03_arithmetic_operators/RectangleArea/src/Task.java
+++ courseSection/m03_arithmetic_operators/RectangleArea/src/Task.java
@@ -1,15 +1,16 @@
 public class Task {
     public static void main(String[] args) {
+
         int width = 10;
         int height = 5;
 
-        // ----Do not change below lines. needed for testing---
-        width = args.length > 0 ? Integer.parseInt(args[0]) : width;
-        height = args.length > 0 ? Integer.parseInt(args[1]) : height;
-        // ------------------------------
+        int area = width * height;
 
+        System.out.println("Area of rectangle=" + area);
 
-        //Write your solution here
-
+        System.out.println("--------------------");
+        System.out.println("|                  |");
+        System.out.println("|                  |");
+        System.out.println("--------------------");
     }
 }
