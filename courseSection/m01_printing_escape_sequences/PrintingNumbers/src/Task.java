ebd8e018ae969ef7b21697c06f14371194551ffa
cherevach-glitch <cherevachandrei@gmail.com>
2025-12-07 12:46:51 +0200

    completed PrintingNumbers  task

--- courseSection/m01_printing_escape_sequences/PrintingNumbers/src/Task.java
+++ courseSection/m01_printing_escape_sequences/PrintingNumbers/src/Task.java
@@ -1,12 +1,19 @@
 public class Task {
     public static void main(String[] args) {
 
-        for (int i = 1; i <= 10; i++) {
-            System.out.println(i);
+        System.out.println(1);
+        System.out.println(2);
+        System.out.println(3);
+        System.out.println(4);
+        System.out.println(5);
 
-            if (i == 5) {
-                System.out.println();
-            }
-        }
+        System.out.println(); // пустий рядок
+
+        System.out.println(6);
+        System.out.println(7);
+        System.out.println(8);
+        System.out.println(9);
+        System.out.println(10);
+
     }
 }
