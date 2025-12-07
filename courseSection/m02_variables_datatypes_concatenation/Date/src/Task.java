44c67af0c98468fcad1971a0bf9f313f353ef7a2
cherevach-glitch <cherevachandrei@gmail.com>
2025-12-08 00:29:46 +0200

    completed Date task

--- courseSection/m02_variables_datatypes_concatenation/Date/src/Task.java
+++ courseSection/m02_variables_datatypes_concatenation/Date/src/Task.java
@@ -1,7 +1,14 @@
 public class Task {
     public static void main(String[] args) {
 
-        //Write your solution here
+        int month = 10;
+        int day = 14;
+        int year = 2023;
 
+        System.out.println("month = " + month);
+        System.out.println("day = " + day);
+        System.out.println("year = " + year);
+
+        System.out.println(month + "/" + day + "/" + year);
     }
 }
