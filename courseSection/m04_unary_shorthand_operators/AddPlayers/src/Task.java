2e10091015b839636f5e5b86f46cd10a4ae2f1c7
cherevach-glitch <cherevachandrei@gmail.com>
2025-12-08 01:12:08 +0200

    completed AddPlayers

--- courseSection/m04_unary_shorthand_operators/AddPlayers/src/Task.java
+++ courseSection/m04_unary_shorthand_operators/AddPlayers/src/Task.java
@@ -1,11 +1,10 @@
 public class Task {
     public static void main(String[] args) {
+
         int players = 5;
-        // Do not change below line:. needed for testing---
-        players = args.length > 0 ? Integer.parseInt(args[0]) : players;
-        //-----------------------------------
 
-        //Write your solution here
+        players += 4;
 
+        System.out.println("players = " + players);
     }
 }
