95c6f7f323018260e318a17a3a7e40ed1ea818ec
cherevach-glitch <cherevachandrei@gmail.com>
2025-12-08 00:36:05 +0200

    completed FlightTicket task

--- courseSection/m02_variables_datatypes_concatenation/FlightTicket/src/Task.java
+++ courseSection/m02_variables_datatypes_concatenation/FlightTicket/src/Task.java
@@ -1,7 +1,18 @@
 public class Task {
     public static void main(String[] args) {
 
-        // Write your solution here
+        String city1 = "Chicago";
+        String city2 = "San Fransisco";
 
+        double oneWayFare = 643.5;
+
+        char DOLLAR = '\u0024'; // Unicode для $
+
+        System.out.println(
+                "From " + city1 +
+                        " to " + city2 +
+                        " one way cost is " +
+                        DOLLAR + oneWayFare
+        );
     }
 }
