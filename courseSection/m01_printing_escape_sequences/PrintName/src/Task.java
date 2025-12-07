d6d7cc30cbf2019d751f65fd3390ba701e3ff785
cherevach-glitch <cherevachandrei@gmail.com>
2025-12-07 19:11:20 +0200

    completed PrintName task

--- courseSection/m01_printing_escape_sequences/PrintName/src/Task.java
+++ courseSection/m01_printing_escape_sequences/PrintName/src/Task.java
@@ -1,12 +1,7 @@
 public class Task {
     public static void main(String[] args) {
 
-        //Write your solution here
-
-        String firstName = "Alan";
-        String lastName = "Turing";
-
-        System.out.print(lastName + ", " + firstName);
+        System.out.println("Turing, Alan");
 
     }
 }
