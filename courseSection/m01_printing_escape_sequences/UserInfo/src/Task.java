b8659609d09ddfadb158c16f35a745cc29896b3e
cherevach-glitch <cherevachandrei@gmail.com>
2025-12-07 19:31:52 +0200

    completed UserInfo task

--- courseSection/m01_printing_escape_sequences/UserInfo/src/Task.java
+++ courseSection/m01_printing_escape_sequences/UserInfo/src/Task.java
@@ -1,7 +1,15 @@
 public class Task {
     public static void main(String[] args) {
 
-        //Write your solution here
+        System.out.println("First Name: Mike");
+        System.out.println("Last Name: Smith");
+
+        System.out.println();
+
+        System.out.println("Address: 100 Task st");
+        System.out.println("City: Fairfax");
+        System.out.println("State: VA");
+        System.out.println("Country: USA");
 
     }
 }
