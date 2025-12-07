41150a6c5aff79291f2c1b50de49ba769b654b43
cherevach-glitch <cherevachandrei@gmail.com>
2025-12-08 00:31:53 +0200

    completed Date task

--- courseSection/m02_variables_datatypes_concatenation/Email/src/Task.java
+++ courseSection/m02_variables_datatypes_concatenation/Email/src/Task.java
@@ -1,7 +1,12 @@
 public class Task {
     public static void main(String[] args) {
 
-        //Write your solution here
+        String firstName = "John";
+        String lastName = "Smith";
+        String company = "amazon";
 
+        String email = firstName + "." + lastName + "@" + company + ".com";
+
+        System.out.println("email = " + email);
     }
 }
