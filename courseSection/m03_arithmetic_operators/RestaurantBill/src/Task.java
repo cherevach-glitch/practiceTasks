c64aca3aff39153408210d3698749b2a83c10629
cherevach-glitch <cherevachandrei@gmail.com>
2025-12-08 01:03:13 +0200

    completed RestaurantBill

--- courseSection/m03_arithmetic_operators/RestaurantBill/src/Task.java
+++ courseSection/m03_arithmetic_operators/RestaurantBill/src/Task.java
@@ -2,14 +2,15 @@ public class Task {
     public static void main(String[] args) {
 
         double bill = 50.0;
-        double tipPercentage = 10;
+        double tipPercentage = 10.0;
 
-        // ----Do not change below lines. needed for testing---
-        bill = args.length > 0 ? Double.parseDouble(args[0]) : bill;
-        tipPercentage = args.length > 0 ? Double.parseDouble(args[1]) : tipPercentage;
-        //--------------------------------
+        double tip = (tipPercentage / 100) * bill;
+        double tax = bill * 0.05;
+        double totalBill = bill + tip + tax;
 
-        //Write your solution here
-
+        System.out.println("bill = " + bill);
+        System.out.println("tax = " + tax);
+        System.out.println("tip = " + tip);
+        System.out.println("totalBill = " + totalBill);
     }
 }
