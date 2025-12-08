31fa76cf6f22236621f935c4eee9a99385b52bda
cherevach-glitch <cherevachandrei@gmail.com>
2025-12-08 13:20:25 +0200

    completed LeapYear task

--- courseSection/m05_relational_logical_operators/LeapYear/src/Task.java
+++ courseSection/m05_relational_logical_operators/LeapYear/src/Task.java
@@ -1,12 +1,10 @@
 public class Task {
     public static void main(String[] args) {
 
-        int year = 1900;
-        // ----Do not change below lines. needed for testing---
-        year = args.length > 0 ? Integer.parseInt(args[0]) : year;
-        //--------------------------------
+        int year = 2000;
 
-        //Write your solution here
+        boolean isLeapYear = (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
 
+        System.out.println("Is " + year + " a leap year? " + isLeapYear);
     }
 }
