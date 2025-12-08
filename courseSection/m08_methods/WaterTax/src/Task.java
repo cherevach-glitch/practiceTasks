df0a47293817dcceb477820725fdf592c8a01b13
cherevach-glitch <cherevachandrei@gmail.com>
2025-12-08 21:14:46 +0200

    completed

--- courseSection/m08_methods/WaterTax/src/Task.java
+++ courseSection/m08_methods/WaterTax/src/Task.java
@@ -1,15 +1,26 @@
 public class Task {
+
+    public static double waterTax(double units) {
+
+        double bill;
+
+        if (units <= 50) {
+            bill = units * 0.60;
+        } else if (units <= 100) {
+            bill = units * 0.90;
+        } else if (units <= 150) {
+            bill = units * 0.90 + 50;
+        } else {
+            bill = units * 0.90 + 100;
+        }
+
+        return bill;
+    }
+
     public static void main(String[] args) {
 
+        System.out.println(waterTax(50));
         System.out.println(waterTax(55));
-
+        System.out.println(waterTax(151));
     }
-
-    public static double waterTax(int units){
-
-        //Write your solution here
-
-    }
-
-
 }
