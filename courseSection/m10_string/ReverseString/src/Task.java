ef836a6d66c69627b113348f43a7b8ea745a2395
cherevach-glitch <cherevachandrei@gmail.com>
2025-12-08 20:54:21 +0200

    completed

--- courseSection/m10_string/ReverseString/src/Task.java
+++ courseSection/m10_string/ReverseString/src/Task.java
@@ -1,14 +1,18 @@
-
 public class Task {
+
     public static void main(String[] args) {
-        System.out.println(reverse("hi"));
         System.out.println(reverse("java"));
+        System.out.println(reverse("Hello"));
     }
 
-    public static String reverse(String str){
+    public static String reverse(String str) {
+        String result = "";
 
-        //Write your solution here
+        for (int i = str.length() - 1; i >= 0; i--) {
+            result += str.charAt(i);
+        }
 
+        return result;
     }
 
 }
--- courseSection/m10_string/ShoppingList/src/Task.java
+++ courseSection/m10_string/ShoppingList/src/Task.java
@@ -1,29 +1,39 @@
 
 public class Task {
+
     public static void main(String[] args) {
-        System.out.println(isPalindrome("racecar"));
-        System.out.println(isPalindrome("anna"));
-        System.out.println(isPalindrome(1001));
+        System.out.println(isPalindrome("level"));
+        System.out.println(isPalindrome("java"));
+
+        System.out.println(isPalindrome(12321));
+        System.out.println(isPalindrome(12345));
     }
 
     public static boolean isPalindrome(String str) {
+        int left = 0;
+        int right = str.length() - 1;
 
-        //Write your solution here
+        while (left < right) {
+            if (str.charAt(left) != str.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
 
         return true;
     }
 
     public static boolean isPalindrome(int num) {
-        if (num < 0) {
-            return false;  // Negative numbers are not palindromes
-        }
-
-        int reversed = 0;
         int original = num;
+        int reversed = 0;
 
-       //Write your solution here
+        while (num > 0) {
+            int digit = num % 10;
+            reversed = reversed * 10 + digit;
+            num /= 10;
+        }
 
         return original == reversed;
     }
-
 }
