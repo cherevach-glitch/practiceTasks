2af8e724b25d230ba5e942d00dd8e08883a345d8
cherevach-glitch <cherevachandrei@gmail.com>
2025-12-08 20:30:52 +0200

    completed

--- courseSection/m10_string/FormatName/src/Task.java
+++ courseSection/m10_string/FormatName/src/Task.java
@@ -1,14 +1,22 @@
-
 public class Task {
+
     public static void main(String[] args) {
-        System.out.println(fixFormat("CADEN ARROYO"));
-        System.out.println(fixFormat("jaX wOLF"));
+
+        System.out.println(formatName("CADEN ARROYO"));
+        System.out.println(formatName("zOIe neLSOn"));
+        System.out.println(formatName("Mara Blair"));
+        System.out.println(formatName("jax wOLF"));
     }
 
-    public static String fixFormat(String name) {
+    public static String formatName(String name) {
 
-        //Write your solution here
+        String[] words = name.toLowerCase().split(" ");
+        String result = "";
 
+        for (String word : words) {
+            result += word.substring(0, 1).toUpperCase() + word.substring(1) + " ";
+        }
+
+        return result.trim();
     }
-
 }
