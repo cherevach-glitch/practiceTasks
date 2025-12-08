1d5a138ca88e22fe31cd152447e06d9af6dc2058
cherevach-glitch <cherevachandrei@gmail.com>
2025-12-08 21:09:06 +0200

    completed

--- courseSection/m08_methods/ProfitOrLoss/src/Task.java
+++ courseSection/m08_methods/ProfitOrLoss/src/Task.java
@@ -1,14 +1,20 @@
 public class Task {
+
+    public static String profit(int buyPrice, int sellPrice) {
+
+        if (sellPrice > buyPrice) {
+            return "profit";
+        } else if (sellPrice < buyPrice) {
+            return "loss";
+        } else {
+            return "no loss";
+        }
+    }
+
     public static void main(String[] args) {
 
-        System.out.println(profit(1, 10));
-
+        System.out.println(profit(100, 1500));
+        System.out.println(profit(20, 5));
+        System.out.println(profit(100, 100));
     }
-
-    public static String profit(int buyPrice, int sellPrice){
-
-        //Write your solution here
-
-    }
-
 }
