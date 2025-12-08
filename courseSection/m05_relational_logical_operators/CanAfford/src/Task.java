9f56d5d83d57c078f861a7be133c0803d7ad76cb
cherevach-glitch <cherevachandrei@gmail.com>
2025-12-08 13:08:38 +0200

    completed   CanAfford task

--- courseSection/m05_relational_logical_operators/CanAfford/src/Task.java
+++ courseSection/m05_relational_logical_operators/CanAfford/src/Task.java
@@ -1,14 +1,11 @@
 public class Task {
     public static void main(String[] args) {
-        double accountBalance =  1235;
+
+        double accountBalance = 350.0;
         double itemPrice = 254.99;
 
-        // ----Do not change below lines. needed for testing---
-        accountBalance = args.length> 0 ? Double.parseDouble(args[0]): accountBalance;
-        itemPrice = args.length> 0 ? Double.parseDouble(args[1]): itemPrice;
-        //--------------------------------
+        boolean canAfford = accountBalance >= itemPrice;
 
-        //Write your solution here
-
+        System.out.println("canAfford = " + canAfford);
     }
 }
