93392256e6e4af1183a8602afcac2835ac8f008e
cherevach-glitch <cherevachandrei@gmail.com>
2025-12-08 21:39:05 +0200

    completed

--- courseSection/m06_selection_statements/PositiveNegativeOrZero/src/Task.java
+++ courseSection/m06_selection_statements/PositiveNegativeOrZero/src/Task.java
@@ -1,13 +1,15 @@
 public class Task {
+
     public static void main(String[] args) {
 
-        int num = 1;
+        int num = -6;
 
-        // ----Do not change below lines. needed for testing---
-        num = args.length > 0 ? Integer.parseInt(args[0]) : num;
-        //--------------------------------
-
-        //Write your solution here
-
+        if (num > 0) {
+            System.out.println("positive");
+        } else if (num < 0) {
+            System.out.println("negative");
+        } else {
+            System.out.println("zero");
+        }
     }
 }
