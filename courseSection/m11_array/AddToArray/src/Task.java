ad73c6113c823d9530b170ea8c948b711f611bcc
cherevach-glitch <cherevachandrei@gmail.com>
2025-12-08 20:02:45 +0200

    completed AddToArray

--- courseSection/m11_array/AddToArray/src/Task.java
+++ courseSection/m11_array/AddToArray/src/Task.java
@@ -1,15 +1,26 @@
 import java.util.Arrays;
 
 public class Task {
+
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(addToArray(new int[]{2, 1, 2, 3, 4}, 10)));
-        System.out.println(Arrays.toString(addToArray(new int[]{1, 3, 5, 7, 9}, 21)));
+
+        int[] arr1 = {2, 1, 2, 3, 4};
+        System.out.println(Arrays.toString(addToArray(arr1, 10)));
+
+        int[] arr2 = {1, 3, 5, 7, 9};
+        System.out.println(Arrays.toString(addToArray(arr2, 21)));
     }
 
-    public static int[] addToArray(int[] nums, int newElement) {
+    public static int[] addToArray(int[] numbers, int extraNumber) {
 
-        //Write your solution here
+        int[] newArray = new int[numbers.length + 1];
 
+        for (int i = 0; i < numbers.length; i++) {
+            newArray[i] = numbers[i];
+        }
+
+        newArray[newArray.length - 1] = extraNumber;
+
+        return newArray;
     }
-
 }
