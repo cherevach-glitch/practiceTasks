45d0921cb6427df4d6d8ceea3d37ef924c7797d2
cherevach-glitch <cherevachandrei@gmail.com>
2025-12-08 20:32:50 +0200

    completed

--- courseSection/m10_string/JavaAndPhython/src/Task.java
+++ courseSection/m10_string/JavaAndPhython/src/Task.java
@@ -1,19 +1,32 @@
 
 public class Task {
+
     public static void main(String[] args) {
+
         System.out.println(sameCount("We study java not python"));
         System.out.println(sameCount("What's the difference between java, javascript and python?"));
         System.out.println(sameCount("java python hello python world python"));
-
     }
 
-    public static boolean sameCount(String str) { // We study java not python
+    public static boolean sameCount(String str) {
 
-        int java = 0;
-        int python = 0;
-        int length = str.length();
-        //Write your solution here
+        str = str.toLowerCase();
 
-        return java == python;
+        int javaCount = 0;
+        int pythonCount = 0;
+
+        for (int i = 0; i <= str.length() - 4; i++) {
+            if (str.substring(i, i + 4).equals("java")) {
+                javaCount++;
+            }
+        }
+
+        for (int i = 0; i <= str.length() - 6; i++) {
+            if (str.substring(i, i + 6).equals("python")) {
+                pythonCount++;
+            }
+        }
+
+        return javaCount == pythonCount;
     }
 }
