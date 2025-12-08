7db1205a3180f6d8496eb6f3284a283d81964c05
cherevach-glitch <cherevachandrei@gmail.com>
2025-12-08 13:22:28 +0200

    completed Alphabet task

--- courseSection/m07_loops/Alphabet/src/Task.java
+++ courseSection/m07_loops/Alphabet/src/Task.java
@@ -1,7 +1,14 @@
 public class Task {
     public static void main(String[] args) {
 
-        //Write your solution here
+        for (char c = 'A'; c <= 'Z'; c++) {
+            System.out.print(c);
+        }
 
+        System.out.println();
+
+        for (char c = 'z'; c >= 'a'; c--) {
+            System.out.print(c);
+        }
     }
 }
