d2b4e359708007b0027da865555a4cbd7e79d876
cherevach-glitch <cherevachandrei@gmail.com>
2025-12-08 13:31:48 +0200

    completed UtopianTree task

--- courseSection/m07_loops/UtopianTree/src/Task.java
+++ courseSection/m07_loops/UtopianTree/src/Task.java
@@ -2,17 +2,21 @@ public class Task {
     public static void main(String[] args) {
 
         int treeSize = 0;
-        int growth = 0;
 
         for (int year = 1; year <= 10; year++) {
-            // Determine the growth per year based on the year. Then add to treeSize
 
-            //Write your solution here
+            int growth;
 
-            // Print the growth and current size of the tree for each year
-            System.out.println("year " + year + " growth " + growth + " cm");
-            System.out.println("tree size: " + treeSize);
+            if (year <= 3) {
+                growth = 1;
+            } else {
+                growth = 2;
+            }
+
+            treeSize += growth;
+
+            System.out.println("year " + year + " - growth " + growth + " cm");
+            System.out.println("tree size: " + treeSize + " cm");
         }
-
     }
 }
