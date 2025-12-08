0a93ce37b3746ebc4678c27dbb650f2500d28559
cherevach-glitch <cherevachandrei@gmail.com>
2025-12-08 21:17:55 +0200

    completed

--- courseSection/m06_selection_statements/ApartmentLeasing/src/Task.java
+++ courseSection/m06_selection_statements/ApartmentLeasing/src/Task.java
@@ -1,15 +1,27 @@
+import java.util.Scanner;
+
 public class Task {
+
     public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
 
         System.out.println("Welcome to Cydeo Apartments!");
         System.out.println("How many bedrooms are you looking for?");
 
-        int numberOfBedrooms = 1;
+        int numberOfBedrooms = input.nextInt();
 
-        // ----Do not change below lines. needed for testing---
-        numberOfBedrooms = args.length > 0 ? Integer.parseInt(args[0]) : numberOfBedrooms;
-        //--------------------------------
-
-        //Write your solution here
+        if (numberOfBedrooms == 1) {
+            System.out.println("One Bedroom Selected");
+            System.out.println("Starting Price: 1100");
+        } else if (numberOfBedrooms == 2) {
+            System.out.println("Two Bedroom Selected");
+            System.out.println("Starting Price: 1850");
+        } else if (numberOfBedrooms == 3) {
+            System.out.println("Three Bedroom Selected");
+            System.out.println("Starting Price: 2550");
+        } else {
+            System.out.println("Sorry, we do not offer that many bedrooms");
+        }
     }
 }
