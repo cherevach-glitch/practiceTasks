d8a7dd3667095ab72f5680701c666d17581ce5be
cherevach-glitch <cherevachandrei@gmail.com>
2025-12-08 20:18:25 +0200

    completed ShortestWords

--- courseSection/m11_array/ShortestWords/src/Task.java
+++ courseSection/m11_array/ShortestWords/src/Task.java
@@ -1,15 +1,41 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Task {
+
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(shortestWords("olive, fish, pursuit, old, warning, python, java, coffee, cat, ray")));
-        System.out.println(Arrays.toString(shortestWords("spare, decrease, morning, article, chaos, orbit, acceptance, lodge, tournament, trade")));
+
+        System.out.println(Arrays.toString(shortestWords(
+                "olive, fish, pursuit, old, warning, python, java, coffee, cat, ray"
+        )));
+
+
+        System.out.println(Arrays.toString(shortestWords(
+                "spare, decrease, morning, article, chaos, orbit, acceptance, lodge, tournament, trade"
+        )));
+
     }
 
-    public static String[] shortestWords(String str){
+    public static String[] shortestWords(String str) {
 
-        //Write your solution here
+        String[] words = str.split(", ");
+        int minLength = words[0].length();
 
+        for (String word : words) {
+            if (word.length() < minLength) {
+                minLength = word.length();
+            }
+        }
+
+        ArrayList<String> result = new ArrayList<>();
+
+        for (String word : words) {
+            if (word.length() == minLength) {
+                result.add(word);
+            }
+        }
+
+
+        return result.toArray(new String[0]);
     }
-
 }
