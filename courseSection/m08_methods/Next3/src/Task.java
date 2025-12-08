25956adeb0f2d9f70f20dd9e944889520515674e
cherevach-glitch <cherevachandrei@gmail.com>
2025-12-08 21:07:50 +0200

    completed

--- courseSection/m08_methods/Next3/src/Task.java
+++ courseSection/m08_methods/Next3/src/Task.java
@@ -1,15 +1,13 @@
 public class Task {
+
+    public static void next3(int n) {
+
+        System.out.println((n + 1) + " " + (n + 2) + " " + (n + 3));
+    }
+
     public static void main(String[] args) {
 
         next3(1);
-
+        next3(10);
     }
-
-    public static void next3(int n){
-
-        //Write your solution here
-
-    }
-
-
 }
