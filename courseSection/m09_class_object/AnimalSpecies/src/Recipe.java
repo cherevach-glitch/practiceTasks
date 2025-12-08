b2ae0704a50872829a5db4c8d717a4a6a287529f
cherevach-glitch <cherevachandrei@gmail.com>
2025-12-08 13:38:22 +0200

    completed Recipe task

--- courseSection/m09_class_object/AnimalSpecies/src/Recipe.java
+++ courseSection/m09_class_object/AnimalSpecies/src/Recipe.java
@@ -1,5 +1,18 @@
 public class Recipe {
 
-    //Write your solution here
+    String name;
+    String ingredients;
+    int servingSize;
+    double cost;
 
+    public double costPerPerson() {
+        return cost / servingSize;
+    }
+
+    @Override
+    public String toString() {
+        return "Recipe for " + name + " will require these ingredients:\n"
+                + ingredients + "\n"
+                + "This dish will serve " + servingSize + " people and cost a total of $ " + cost + " to make";
+    }
 }
