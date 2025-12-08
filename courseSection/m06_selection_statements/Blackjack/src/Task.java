12d9348ce4bd96de995b6acc4e8fdbadd3e7496b
cherevach-glitch <cherevachandrei@gmail.com>
2025-12-08 21:18:59 +0200

    completed

--- courseSection/m06_selection_statements/Blackjack/src/Task.java
+++ courseSection/m06_selection_statements/Blackjack/src/Task.java
@@ -1,14 +1,20 @@
 public class Task {
+
     public static void main(String[] args) {
-        int house = 55;
-        int player = 85;
 
-        // ----Do not change below lines. needed for testing---
-        house = args.length > 0 ? Integer.parseInt(args[0]) : house;
-        player = args.length > 0 ? Integer.parseInt(args[1]) : player;
-        //--------------------------------
+        int house = 8;
+        int player = 21;
 
-        //Write your solution here
-
+        if (player > 21) {
+            System.out.println("player bust");
+        } else if (player == house) {
+            System.out.println("its a tie");
+        } else if (player == 21) {
+            System.out.println("player win");
+        } else if (house > player) {
+            System.out.println("player lose");
+        } else {
+            System.out.println("player win");
+        }
     }
 }
