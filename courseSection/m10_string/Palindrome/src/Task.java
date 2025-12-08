1ad03b450b37487850ec25b8b099dc4e55873057
cherevach-glitch <cherevachandrei@gmail.com>
2025-12-08 20:35:11 +0200

    completed

--- courseSection/m10_string/Palindrome/src/Task.java
+++ courseSection/m10_string/Palindrome/src/Task.java
@@ -1,29 +1,44 @@
 
 public class Task {
+
     public static void main(String[] args) {
+
         System.out.println(isPalindrome("racecar"));
-        System.out.println(isPalindrome("anna"));
-        System.out.println(isPalindrome(1001));
+        System.out.println(isPalindrome("hello"));
+
+        System.out.println(isPalindrome(121));
+        System.out.println(isPalindrome(123));
     }
+
 
     public static boolean isPalindrome(String str) {
 
-        //Write your solution here
+        str = str.toLowerCase();
+        int start = 0;
+        int end = str.length() - 1;
+
+        while (start < end) {
+            if (str.charAt(start) != str.charAt(end)) {
+                return false;
+            }
+            start++;
+            end--;
+        }
 
         return true;
     }
 
     public static boolean isPalindrome(int num) {
-        if (num < 0) {
-            return false;  // Negative numbers are not palindromes
-        }
 
-        int reversed = 0;
         int original = num;
+        int reversed = 0;
 
-       //Write your solution here
+        while (num > 0) {
+            int digit = num % 10;
+            reversed = reversed * 10 + digit;
+            num = num / 10;
+        }
 
         return original == reversed;
     }
-
 }
