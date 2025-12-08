19b11bebcdfc8b2e202434de493893c681ebb0fa
cherevach-glitch <cherevachandrei@gmail.com>
2025-12-08 19:41:58 +0200

    completed Nanuk

--- courseSection/m12_arraylist/Nanuk/src/Task.java
+++ courseSection/m12_arraylist/Nanuk/src/Task.java
@@ -1,22 +1,35 @@
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 public class Task {
-    public static void main(String[] args) {
-        System.out.println("nanuk([\"1\",\"2\",\"3\",\"5\"],0,5) -> " + hunt(new ArrayList<>(Arrays.asList("1","2","3","5")), 0, 5));
-        System.out.println("nanuk([\"1\",\"nanuk\",\"3\",\"5\"],0,5) -> " + hunt(new ArrayList<>(Arrays.asList("1","nanuk","3","5")), 0, 5));
-        System.out.println("nanuk([\"1\",\"3\"],0,5) -> " + hunt(new ArrayList<>(Arrays.asList("1","3")), 0, 5));
-        System.out.println("nanuk([\"1\",\"10\",\"3\",\"nanuk\"],2,7) -> " + hunt(new ArrayList<>(Arrays.asList("1","10","3","nanuk")), 2, 7));
-        System.out.println("nanuk([\"1\",\"10\",\"3\",\"nanuk\",\"nanuk\"],2,7) -> " + hunt(new ArrayList<>(Arrays.asList("1","10","3","nanuk","nanuk")), 2, 7));
-        System.out.println("nanuk([\"1\",\"10\",\"3\",\"nanuk\",\"nanuk\",\"nanuk\"],2,7) -> " + hunt(new ArrayList<>(Arrays.asList("1","10","3","nanuk","nanuk","nanuk")), 2, 7));
 
+    public static void main(String[] args) {
+
+        System.out.println(hunt(new ArrayList<>(Arrays.asList("1","2","3","5")), 0, 5)); // true
+        System.out.println(hunt(new ArrayList<>(Arrays.asList("1","nanuk","3","5")), 0, 5)); // false
+        System.out.println(hunt(new ArrayList<>(Arrays.asList("1","3")), 0, 5)); // false
+        System.out.println(hunt(new ArrayList<>(Arrays.asList("1","10","3","nanuk")), 2, 7)); // true
+        System.out.println(hunt(new ArrayList<>(Arrays.asList("1","10","3","nanuk","nanuk")), 2, 7)); // true
+        System.out.println(hunt(new ArrayList<>(Arrays.asList("1","10","3","nanuk","nanuk","nanuk")), 2, 7)); // false
     }
 
     public static boolean hunt(ArrayList<String> result, int wayStones, int boast) {
 
-        //Write your solution here
+        int totalCatch = 0;
 
+        for (String each : result) {
+
+            if (each.equals("nanuk")) {
+                if (wayStones > 0) {
+                    wayStones--;
+                } else {
+                    return false;
+                }
+            } else {
+                totalCatch += Integer.parseInt(each);
+            }
+        }
+
+        return totalCatch >= boast;
     }
-
 }
