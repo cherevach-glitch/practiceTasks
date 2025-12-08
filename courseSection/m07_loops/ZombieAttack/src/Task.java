8e682a8bc9f43bff3eb619ad9f985e83714a6de0
cherevach-glitch <cherevachandrei@gmail.com>
2025-12-08 13:34:47 +0200

    completed ZombieAttack task

--- courseSection/m07_loops/ZombieAttack/src/Task.java
+++ courseSection/m07_loops/ZombieAttack/src/Task.java
@@ -1,13 +1,15 @@
 public class Task {
     public static void main(String[] args) {
 
-        int inhabitants = 6;
+        int inhabitants = 20;
+        int day = 0;
 
-        // ----Do not change below lines. needed for testing---
-        inhabitants = args.length > 0 ? Integer.parseInt(args[0]) : inhabitants;
-        //--------------------------------
+        while (inhabitants > 0) {
+            System.out.println("Day " + day + " [" + inhabitants + "]");
+            inhabitants = inhabitants / 2;
+            day++;
+        }
 
-        //Write your solution here
-
+        System.out.println("---- EXTINCT ----");
     }
 }
