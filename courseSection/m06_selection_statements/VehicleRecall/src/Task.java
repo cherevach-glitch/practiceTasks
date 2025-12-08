546f6baf082250b72b516cc61bf2224e207a6bef
cherevach-glitch <cherevachandrei@gmail.com>
2025-12-08 21:44:48 +0200

    completed

--- courseSection/m06_selection_statements/VehicleRecall/src/Task.java
+++ courseSection/m06_selection_statements/VehicleRecall/src/Task.java
@@ -1,14 +1,16 @@
 public class Task {
+
     public static void main(String[] args) {
 
         int modelYear = 2002;
 
-        // ----Do not change below lines. needed for testing---
-        modelYear = args.length > 0 ? Integer.parseInt(args[0]) : modelYear;
-        //--------------------------------
+        if ((modelYear >= 2001 && modelYear <= 2002)
+                || (modelYear >= 2004 && modelYear <= 2006)
+                || (modelYear >= 2015 && modelYear <= 2018)) {
 
-        //Write your solution here
-
-
+            System.out.println("RECALL");
+        } else {
+            System.out.println("NOT RECALL");
+        }
     }
 }
