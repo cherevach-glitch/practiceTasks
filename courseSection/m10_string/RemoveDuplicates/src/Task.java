7028adc6f69a846c67d2e81d8f9446d6715552ae
cherevach-glitch <cherevachandrei@gmail.com>
2025-12-08 20:45:55 +0200

    completed

--- courseSection/m10_string/RemoveDuplicates/src/Task.java
+++ courseSection/m10_string/RemoveDuplicates/src/Task.java
@@ -1,17 +1,38 @@
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Task {
     public static void main(String[] args) {
-        System.out.println(uniqueChars("java"));
-        System.out.println(uniqueChars("mama"));
+
+        Scanner input = new Scanner(System.in);
+        ArrayList<String> guests = new ArrayList<>();
+
+        String answer;
+
+        do {
+            System.out.println("Please enter the guest's name:");
+            String name = input.nextLine();
+
+
+            if (!guests.contains(name)) {
+                guests.add(name);
+            }
+
+            System.out.println("Do you want to enter another guest's name?");
+            answer = input.nextLine();
+
+        } while (answer.equalsIgnoreCase("yes"));
+
+
+        System.out.print("Guests' list: ");
+        for (int i = 0; i < guests.size(); i++) {
+            if (i > 0) {
+                System.out.print(", ");
+            }
+            System.out.print(guests.get(i));
+        }
     }
-
-    public static String uniqueChars(String str) {
-
-        String unique = "";
-
-        //Write your solution here
-
-        return unique;
-    }
-
 }
+
+
+
