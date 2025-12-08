eb21cccdb5a69a0de85b4630f3c34187bedf5035
cherevach-glitch <cherevachandrei@gmail.com>
2025-12-08 21:24:10 +0200

    completed

--- courseSection/m06_selection_statements/Finra/src/Task.java
+++ courseSection/m06_selection_statements/Finra/src/Task.java
@@ -1,12 +1,17 @@
 public class Task {
+
     public static void main(String[] args) {
 
-        int number = 93;
+        int number = 15;
 
-        // ----Do not change below lines. needed for testing---
-        number = args.length > 0 ? Integer.parseInt(args[0]) : 93;
-        //--------------------------------
-
-        //Write your solution here
+        if (number % 3 == 0 && number % 5 == 0) {
+            System.out.println("FINRA");
+        } else if (number % 3 == 0) {
+            System.out.println("FIN");
+        } else if (number % 5 == 0) {
+            System.out.println("RA");
+        } else {
+            System.out.println(number);
+        }
     }
 }
