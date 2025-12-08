7d142ade324a81b4ea8116133850968d933c1803
cherevach-glitch <cherevachandrei@gmail.com>
2025-12-08 19:48:47 +0200

    completed SalesReport

--- courseSection/m12_arraylist/SalesReport/src/Task.java
+++ courseSection/m12_arraylist/SalesReport/src/Task.java
@@ -1,24 +1,38 @@
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 public class Task {
+
     public static void main(String[] args) {
-        ArrayList<Double> list = new ArrayList<>(List.of(300000.0, 200000.0, 250000.0, 250000.0));
-        System.out.println(calculateSales(list));
 
-        list = new ArrayList<>(List.of(300000.0, 200000.0, 250000.0, 1000000.0, 15000000.0));
-        System.out.println(calculateSales(list));
+        System.out.println(calculateSales(new ArrayList<>(Arrays.asList(
+                300000.0, 200000.0, 250000.0, 250000.0
+        ))));
 
-        list = new ArrayList<>(List.of(3000000.0, 5000.0, 250000.0, 1000000.0, 3500000.0));
-        System.out.println(calculateSales(list));
+        System.out.println(calculateSales(new ArrayList<>(Arrays.asList(
+                1200000.0, 1300000.0, 1400000.0, 1500000.0
+        ))));
 
+
+        System.out.println(calculateSales(new ArrayList<>(Arrays.asList(
+                3000000.0, 5000.0, 250000.0, 1000000.0, 350000.0
+        ))));
     }
 
     public static boolean calculateSales(ArrayList<Double> sales) {
 
-        //Write your solution here
+        double total = 0;
 
+        for (double each : sales) {
+
+
+            if (each < 30000) {
+                return false;
+            }
+
+            total += each;
+        }
+
+        return total >= 5_000_000;
     }
-
 }
