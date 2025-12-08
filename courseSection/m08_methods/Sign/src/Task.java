d97e2f45e981e216ffc89acc9f11d9057c07cd7e
cherevach-glitch <cherevachandrei@gmail.com>
2025-12-08 21:10:17 +0200

    completed

--- courseSection/m08_methods/Sign/src/Task.java
+++ courseSection/m08_methods/Sign/src/Task.java
@@ -1,12 +1,20 @@
 public class Task {
+
+    public static void sign(int n) {
+
+        if (n > 0) {
+            System.out.println("positive");
+        } else if (n < 0) {
+            System.out.println("negative");
+        } else {
+            System.out.println("zero");
+        }
+    }
+
     public static void main(String[] args) {
 
         sign(5);
-
+        sign(-30);
+        sign(0);
     }
-
-    public static void sign(int n){
-        //Write your solution here
-    }
-
 }
