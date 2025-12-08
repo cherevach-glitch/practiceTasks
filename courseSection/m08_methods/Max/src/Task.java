a8ab228605e3ed940d0f7bd6084070aa6c970cb2
cherevach-glitch <cherevachandrei@gmail.com>
2025-12-08 21:06:45 +0200

    completed

--- courseSection/m08_methods/Max/src/Task.java
+++ courseSection/m08_methods/Max/src/Task.java
@@ -1,14 +1,17 @@
 public class Task {
+
+    public static int max(int a, int b) {
+        if (a > b) {
+            return a;
+        } else {
+            return b;
+        }
+    }
+
     public static void main(String[] args) {
 
         System.out.println(max(1, 10));
-
+        System.out.println(max(20, 19));
+        System.out.println(max(11, 5));
     }
-
-    public static int max(int n, int n2){
-
-        //Write your solution here
-
-    }
-
 }
