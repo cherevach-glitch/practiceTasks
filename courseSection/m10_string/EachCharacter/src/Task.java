5530ffa31e2635832382fd3b7799effc812348d7
cherevach-glitch <cherevachandrei@gmail.com>
2025-12-08 20:29:47 +0200

    completed

--- courseSection/m10_string/EachCharacter/src/Task.java
+++ courseSection/m10_string/EachCharacter/src/Task.java
@@ -1,14 +1,33 @@
 
 public class Task {
+
     public static void main(String[] args) {
+
         System.out.println(countLetters("aaabbcccc"));
         System.out.println(countLetters("appleeess"));
     }
 
-    public static String countLetters(String str){
+    public static String countLetters(String str) {
 
         String result = "";
-        //Write your solution here
+        boolean[] used = new boolean[str.length()];
+
+        for (int i = 0; i < str.length(); i++) {
+
+            if (used[i]) continue;
+
+            char current = str.charAt(i);
+            int count = 1;
+
+            for (int j = i + 1; j < str.length(); j++) {
+                if (str.charAt(j) == current) {
+                    count++;
+                    used[j] = true;
+                }
+            }
+
+            result += count + "" + current;
+        }
 
         return result;
     }
