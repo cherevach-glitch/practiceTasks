388c7bf74f1542595a1e00e4636ccd2fa4cb2235
cherevach-glitch <cherevachandrei@gmail.com>
2025-12-08 20:57:35 +0200

    completed

--- courseSection/m10_string/Vowels/src/Task.java
+++ courseSection/m10_string/Vowels/src/Task.java
@@ -1,18 +1,24 @@
 public class Task {
-    public static void main(String[] args) {
 
-        String word = "apple";
+    public static void printVowels(String word) {
 
-        // ----Do not change below lines. needed for testing---
-        word = args.length > 0 ? args[0] : word;
-        //--------------------------------
+        for (int i = 0; i < word.length(); i++) {
+            char ch = word.charAt(i);
 
-        String vowels = "aeiou";
-        for(int i = 0; i < word.length(); i++){
-            String eachChar = word.substring(i, i+1);
-            if(vowels.contains(eachChar)){
-                System.out.print(eachChar);
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+                System.out.print(ch);
             }
         }
+    }
+
+    public static void main(String[] args) {
+
+        printVowels("howdyho");
+        System.out.println();
+
+        printVowels("java");
+        System.out.println();
+
+        printVowels("apple");
     }
 }
