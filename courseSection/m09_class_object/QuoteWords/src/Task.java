21b5aa3a3f6b6fcb074d6cbbf7bce1e8dfbd63b7
cherevach-glitch <cherevachandrei@gmail.com>
2025-12-08 14:12:30 +0200

    completed QuoteWords task

--- courseSection/m09_class_object/QuoteWords/src/Task.java
+++ courseSection/m09_class_object/QuoteWords/src/Task.java
@@ -3,7 +3,11 @@ import java.util.Scanner;
 public class Task {
     public static void main(String[] args) {
 
-        //Write your solution here
+        Scanner input = new Scanner(System.in);
 
+        String topic1 = input.next();
+        String topic2 = input.next();
+
+        System.out.println("I will learn \"" + topic1 + "\" and \"" + topic2 + "\" at Cydeo.");
     }
 }
