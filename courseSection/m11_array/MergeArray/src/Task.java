dda96cebf4ba18368255840625814f1e1d4b54d2
cherevach-glitch <cherevachandrei@gmail.com>
2025-12-08 20:12:46 +0200

    completed MergeArray

--- courseSection/m11_array/MergeArray/src/Task.java
+++ courseSection/m11_array/MergeArray/src/Task.java
@@ -1,16 +1,40 @@
 import java.util.Arrays;
 
 public class Task {
+
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(mergeArray(new int[]{1, 2, 3}, new int[]{4, 5})));
-        System.out.println(Arrays.toString(mergeArray(new int[]{5, 25, 1}, new int[]{0, 3, 6})));
-        System.out.println(Arrays.toString(mergeArray(new int[]{8}, new int[]{4, 10, 9, 2})));
+
+        int[] a1 = {1, 2, 3};
+        int[] b1 = {4, 5};
+        System.out.println(Arrays.toString(mergeArray(a1, b1)));
+
+
+        int[] a2 = {5, 25, 1};
+        int[] b2 = {0, 3, 6};
+        System.out.println(Arrays.toString(mergeArray(a2, b2)));
+
+        int[] a3 = {8};
+        int[] b3 = {4, 10, 9, 2};
+        System.out.println(Arrays.toString(mergeArray(a3, b3)));
+
     }
 
-    public static int[] mergeArray(int[] nums1, int[] nums2){
+    public static int[] mergeArray(int[] arr1, int[] arr2) {
 
-        //Write your solution here
+        int[] result = new int[arr1.length + arr2.length];
+        int index = 0;
 
+        for (int i = 0; i < Math.max(arr1.length, arr2.length); i++) {
+
+            if (i < arr1.length) {
+                result[index++] = arr1[i];
+            }
+
+            if (i < arr2.length) {
+                result[index++] = arr2[i];
+            }
+        }
+
+        return result;
     }
-
 }
