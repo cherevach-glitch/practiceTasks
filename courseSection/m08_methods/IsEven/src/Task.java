041e5f1e8639e3b0409d0ffbd9eb4e84235955f5
cherevach-glitch <cherevachandrei@gmail.com>
2025-12-08 21:05:27 +0200

    completed

--- courseSection/m08_methods/IsEven/src/Task.java
+++ courseSection/m08_methods/IsEven/src/Task.java
@@ -1,14 +1,14 @@
 public class Task {
+
+    public static boolean isEven(int number) {
+        return number % 2 == 0;
+    }
+
     public static void main(String[] args) {
 
         System.out.println(isEven(4));
-
+        System.out.println(isEven(7));
+        System.out.println(isEven(10));
+        System.out.println(isEven(15));
     }
-
-    public static boolean isEven(int n){
-
-        //Write your solution here
-
-    }
-
 }
