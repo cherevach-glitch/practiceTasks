f11a46df664505c4a5cd327d89e6f3bd867ee8e8
cherevach-glitch <cherevachandrei@gmail.com>
2025-12-08 21:13:27 +0200

    completed

--- courseSection/m08_methods/SimpleRoomBooking/src/Task.java
+++ courseSection/m08_methods/SimpleRoomBooking/src/Task.java
@@ -1,14 +1,29 @@
 public class Task {
-    public static void main(String[] args) {
-
-        System.out.println(simpleRoomBook(true,2,1,2018));
-
-    }
 
     public static boolean simpleRoomBook(boolean isAvailable, int month, int day, int year) {
 
-        //Write your solution here
 
+        if (!isAvailable) {
+            return false;
+        }
+
+        if (year != 2018) {
+            return false;
+        }
+
+        if (month == 7 && day >= 1 && day <= 8) {
+            return false;
+        }
+
+        return true;
     }
 
+    public static void main(String[] args) {
+
+        System.out.println(simpleRoomBook(false, 2, 1, 2018));
+        System.out.println(simpleRoomBook(true, 2, 1, 2018));
+        System.out.println(simpleRoomBook(true, 2, 1, 2022));
+        System.out.println(simpleRoomBook(true, 7, 1, 2018));
+        System.out.println(simpleRoomBook(true, 7, 9, 2018));
+    }
 }
