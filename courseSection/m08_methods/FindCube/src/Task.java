ed75d3c69a54522123c28efc23ed4e3f65716320
cherevach-glitch <cherevachandrei@gmail.com>
2025-12-08 21:03:25 +0200

    completed

--- courseSection/m08_methods/FindCube/src/Task.java
+++ courseSection/m08_methods/FindCube/src/Task.java
@@ -1,12 +1,11 @@
 public class Task {
+
+    public static void cube(int n) {
+        int result = n * n * n;
+        System.out.println(n + " * " + n + " * " + n + " = " + result);
+    }
+
     public static void main(String[] args) {
         cube(5);
     }
-
-    public static void cube(int n){
-
-        //Write your solution here
-
-    }
-
 }
