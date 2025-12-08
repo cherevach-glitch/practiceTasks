e7f44dbe8606c4fa80186399e5ffbd9afb8b1471
cherevach-glitch <cherevachandrei@gmail.com>
2025-12-08 21:27:26 +0200

    completed

--- courseSection/m06_selection_statements/Grades/src/Task.java
+++ courseSection/m06_selection_statements/Grades/src/Task.java
@@ -1,15 +1,22 @@
 public class Task {
+
     public static void main(String[] args) {
 
         int testScore = 85;
-
-        // ----Do not change below lines. needed for testing---
-        testScore = args.length > 0 ? Integer.parseInt(args[0]) : testScore;
-        //--------------------------------
-
         char grade;
 
-        //Write your solution here
+        if (testScore >= 90) {
+            grade = 'A';
+        } else if (testScore >= 80) {
+            grade = 'B';
+        } else if (testScore >= 70) {
+            grade = 'C';
+        } else if (testScore >= 60) {
+            grade = 'D';
+        } else {
+            grade = 'F';
+        }
 
+        System.out.println(grade);
     }
 }
