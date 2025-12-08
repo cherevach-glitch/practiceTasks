f97819d07db1a924787867273140339b13955d40
cherevach-glitch <cherevachandrei@gmail.com>
2025-12-08 21:25:22 +0200

    completed

--- courseSection/m06_selection_statements/GradeLevel/src/Task.java
+++ courseSection/m06_selection_statements/GradeLevel/src/Task.java
@@ -1,12 +1,22 @@
 public class Task {
+
     public static void main(String[] args) {
 
-        int age = 25;
-
-        // ----Do not change below lines. needed for testing---
-        age = args.length > 0 ? Integer.parseInt(args[0]) : age;
-        //--------------------------------
-
-        //Write your solution here
+        int age = 8;
+        if (age < 3) {
+            System.out.println("ineligible");
+        } else if (age <= 4) {
+            System.out.println("preschool");
+        } else if (age == 5) {
+            System.out.println("kindergarten");
+        } else if (age <= 10) {
+            System.out.println("elementary");
+        } else if (age <= 13) {
+            System.out.println("middle school");
+        } else if (age <= 18) {
+            System.out.println("high school");
+        } else {
+            System.out.println("college");
+        }
     }
 }
