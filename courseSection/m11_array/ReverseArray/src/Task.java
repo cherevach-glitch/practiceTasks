7be14a9080e8445b24f4a49686663bc22680d5d1
cherevach-glitch <cherevachandrei@gmail.com>
2025-12-08 20:14:30 +0200

    completed ReverseArray

--- courseSection/m11_array/ReverseArray/src/Task.java
+++ courseSection/m11_array/ReverseArray/src/Task.java
@@ -1,15 +1,27 @@
 import java.util.Arrays;
 
 public class Task {
+
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(reverseArray(new int[]{5, 1, 32, 100, 31, 13})));
-        System.out.println(Arrays.toString(reverseArray(new int[]{7, 325, 4, 94, 15})));
+
+        int[] a1 = {5, 1, 32, 100, 31, 13};
+        System.out.println(Arrays.toString(reverseArray(a1)));
+
+
+        int[] a2 = {7, 325, 4, 94, 15};
+        System.out.println(Arrays.toString(reverseArray(a2)));
+
     }
 
     public static int[] reverseArray(int[] nums) {
 
-        //Write your solution here
+        int[] reversed = new int[nums.length];
+        int index = 0;
 
+        for (int i = nums.length - 1; i >= 0; i--) {
+            reversed[index++] = nums[i];
+        }
+
+        return reversed;
     }
-
 }
