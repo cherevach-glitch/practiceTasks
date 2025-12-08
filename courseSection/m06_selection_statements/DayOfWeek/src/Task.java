4646a9aef5e48d108affd14a819fe327913242b9
cherevach-glitch <cherevachandrei@gmail.com>
2025-12-08 21:22:59 +0200

    completed

--- courseSection/m06_selection_statements/DayOfWeek/src/Task.java
+++ courseSection/m06_selection_statements/DayOfWeek/src/Task.java
@@ -1,12 +1,36 @@
 public class Task {
+
     public static void main(String[] args) {
 
         int day = 1;
+        String dayName;
 
-        // ----Do not change below lines. needed for testing---
-        day = args.length > 0 ? Integer.parseInt(args[0]) : day;
-        //--------------------------------
+        switch (day) {
+            case 1:
+                dayName = "Monday";
+                break;
+            case 2:
+                dayName = "Tuesday";
+                break;
+            case 3:
+                dayName = "Wednesday";
+                break;
+            case 4:
+                dayName = "Thursday";
+                break;
+            case 5:
+                dayName = "Friday";
+                break;
+            case 6:
+                dayName = "Saturday";
+                break;
+            case 7:
+                dayName = "Sunday";
+                break;
+            default:
+                dayName = "No such a day";
+        }
 
-        //Write your solution here
+        System.out.println(dayName);
     }
 }
