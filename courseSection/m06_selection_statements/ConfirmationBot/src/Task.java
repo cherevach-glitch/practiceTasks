11e8820d64523d2e8c0c7a02cfe4d6dbca058b09
cherevach-glitch <cherevachandrei@gmail.com>
2025-12-08 21:21:45 +0200

    completed

--- courseSection/m06_selection_statements/ConfirmationBot/src/Task.java
+++ courseSection/m06_selection_statements/ConfirmationBot/src/Task.java
@@ -1,14 +1,29 @@
+import java.util.Scanner;
+
 public class Task {
+
     public static void main(String[] args) {
 
-        char response = 'y';
+        Scanner input = new Scanner(System.in);
 
-        // ----Do not change below lines. needed for testing---
-        response = args.length > 0 ? args[0].charAt(0) : response;
-        //--------------------------------
+        System.out.println("Enter command:");
+        char response = input.next().charAt(0);
 
+        switch (response) {
+            case 'y':
+                System.out.println("Your request is being processed");
+                break;
 
-        //Write your solution here
+            case 'n':
+                System.out.println("Thank you for your consideration");
+                break;
 
+            case 'h':
+                System.out.println("Sorry, no live agents are currently available");
+                break;
+
+            default:
+                System.out.println("Invalid entry, please try again");
+        }
     }
 }
