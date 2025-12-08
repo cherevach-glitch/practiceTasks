f2682185fd52ccbea5255d478e9332883bfec368
cherevach-glitch <cherevachandrei@gmail.com>
2025-12-08 20:08:51 +0200

    completed FindMax

--- courseSection/m11_array/FindMax/src/Task.java
+++ courseSection/m11_array/FindMax/src/Task.java
@@ -1,14 +1,27 @@
-
 public class Task {
+
     public static void main(String[] args) {
-        System.out.println(findMax(new int[]{2, 4, 2, 3, -2}));
-        System.out.println(findMax(new int[]{1, 33, 5, 7, 9}));
-        System.out.println(findMax(new int[]{-12, -10, -3, -6, -9}));
+
+        int[] nums1 = {2, 4, 2, 3, -2};
+        System.out.println(findMax(nums1)); // 4
+
+        int[] nums2 = {1, 33, 5, 7, 9};
+        System.out.println(findMax(nums2)); // 33
+
+        int[] nums3 = {-12, -10, -3, -6, -9};
+        System.out.println(findMax(nums3)); // -3
     }
 
-    public static int findMax(int[] nums){
+    public static int findMax(int[] nums) {
 
-        //Write your solution here
+        int max = nums[0];
 
+        for (int num : nums) {
+            if (num > max) {
+                max = num;
+            }
+        }
+
+        return max;
     }
 }
