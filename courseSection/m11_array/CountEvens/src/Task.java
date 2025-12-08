b0fdf5d3fb7dc25350af997f56448f26a77c5bc4
cherevach-glitch <cherevachandrei@gmail.com>
2025-12-08 20:07:13 +0200

    completed CountEvens

--- courseSection/m11_array/CountEvens/src/Task.java
+++ courseSection/m11_array/CountEvens/src/Task.java
@@ -1,13 +1,25 @@
 
 public class Task {
+
     public static void main(String[] args) {
-        System.out.println(countEvens(new int[]{2, 1, 2, 3, 4}));
-        System.out.println(countEvens(new int[]{1, 3, 5, 7, 9}));
+
+        int[] nums1 = {2, 1, 2, 3, 4};
+        System.out.println(countEvens(nums1)); // 3
+
+        int[] nums2 = {1, 3, 5, 7, 9};
+        System.out.println(countEvens(nums2)); // 0
     }
 
-    public static int countEvens(int[] nums){
+    public static int countEvens(int[] nums) {
 
-        //Write your solution here
+        int count = 0;
 
+        for (int num : nums) {
+            if (num % 2 == 0) {
+                count++;
+            }
+        }
+
+        return count;
     }
 }
