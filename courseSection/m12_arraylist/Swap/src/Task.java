8800b465d3590c1f2984bc96f28fd0bb52756a9b
cherevach-glitch <cherevachandrei@gmail.com>
2025-12-08 20:00:26 +0200

    completed Swap

--- courseSection/m12_arraylist/Swap/src/Task.java
+++ courseSection/m12_arraylist/Swap/src/Task.java
@@ -2,15 +2,22 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Task {
+
     public static void main(String[] args) {
-        System.out.println("swap([\"one\",\"two\",\"three\"], 0, 2) -> " + swap(new ArrayList<>(Arrays.asList("one", "two", "three")), 0, 2));
-        System.out.println("swap([\"a\", \"c\", \"b\", \"d\", \"e\"], 0, 3) -> " + swap(new ArrayList<>(Arrays.asList("a", "c", "b", "d", "e")), 0, 3));
+
+        ArrayList<String> list1 = new ArrayList<>(Arrays.asList("one", "two", "three"));
+        System.out.println(swap(list1, 0, 2));
+
+        ArrayList<String> list2 = new ArrayList<>(Arrays.asList("a", "c", "b", "d", "e"));
+        System.out.println(swap(list2, 0, 3));
     }
 
     public static ArrayList<String> swap(ArrayList<String> list, int pos1, int pos2) {
 
-        //Write your solution here
+        String temp = list.get(pos1);
+        list.set(pos1, list.get(pos2));
+        list.set(pos2, temp);
 
+        return list;
     }
-
 }
