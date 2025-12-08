94a97b4f600b1454d89c1a77ca36bb4b72c07a95
cherevach-glitch <cherevachandrei@gmail.com>
2025-12-08 20:16:27 +0200

    completed ReversedSentence

--- courseSection/m11_array/ReversedSentence/src/Task.java
+++ courseSection/m11_array/ReversedSentence/src/Task.java
@@ -1,14 +1,23 @@
-
 public class Task {
+
     public static void main(String[] args) {
+
         System.out.println(reverseSentence("Java is fun"));
+
+
         System.out.println(reverseSentence("Hello! How are you?"));
-    }
-
-    public static String reverseSentence(String sentence){
-
-        //Write your solution here
 
     }
 
+    public static String reverseSentence(String sentence) {
+
+        String[] words = sentence.split(" ");
+        String result = "";
+
+        for (int i = words.length - 1; i >= 0; i--) {
+            result += words[i] + " ";
+        }
+
+        return result.trim();
+    }
 }
