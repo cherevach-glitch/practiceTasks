3382260bc9ff45078ea38f569a198ed3458d81ff
cherevach-glitch <cherevachandrei@gmail.com>
2025-12-08 20:06:03 +0200

    completed BiggestWord

--- courseSection/m11_array/BiggestWord/src/Task.java
+++ courseSection/m11_array/BiggestWord/src/Task.java
@@ -1,14 +1,24 @@
-
 public class Task {
+
     public static void main(String[] args) {
-        System.out.println(longestWord(new String[]{"aaa", "bbbbb", "whasstupppp", "longg", "jaaaaavvaaaaaaaaaa"}));
-        System.out.println(longestWord(new String[]{"five5", "six666", "one1111111111111", "2two", "four4444"}));
+
+        String[] words1 = {"aaa", "bbbbb", "whasstupppp", "longg", "jaaaaavvaaaaaaaaaa"};
+        System.out.println(longestWord(words1));
+
+        String[] words2 = {"five5", "six666", "one1111111111111", "2two", "four4444"};
+        System.out.println(longestWord(words2));
     }
 
-    public static String longestWord(String[] words){
+    public static String longestWord(String[] words) {
 
-        //Write your solution here
+        String longest = words[0];
 
+        for (String word : words) {
+            if (word.length() > longest.length()) {
+                longest = word;
+            }
+        }
+
+        return longest;
     }
-
 }
