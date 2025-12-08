192cc8d8f49cafe6499f5056639f50896d412ad5
cherevach-glitch <cherevachandrei@gmail.com>
2025-12-08 13:18:02 +0200

    completed IsLetter task

--- courseSection/m05_relational_logical_operators/IsLetter/src/Task.java
+++ courseSection/m05_relational_logical_operators/IsLetter/src/Task.java
@@ -2,11 +2,9 @@ public class Task {
     public static void main(String[] args) {
 
         char c = 'a';
-        // ----Do not change below lines. needed for testing---
-        c = args.length > 0 ? args[0].charAt(0) : c;
-        //--------------------------------
 
-        //Write your solution here
+        boolean isLetter = (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z');
 
+        System.out.println("isLetter = " + isLetter);
     }
 }
