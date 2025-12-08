9b884acc1ad4b02d400d646c7ed3a3852a857cae
cherevach-glitch <cherevachandrei@gmail.com>
2025-12-08 20:37:35 +0200

    completed

--- courseSection/m10_string/Party/src/Task.java
+++ courseSection/m10_string/Party/src/Task.java
@@ -1,18 +1,30 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Task {
+
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
-        String guestList = "Guests' list: ";
-        String moreGuests = "yes";
+        ArrayList<String> guests = new ArrayList<>();
+
+        String answer;
 
         do {
-            //Write your solution here
+            System.out.println("Please enter the guest's name:");
+            String name = input.nextLine();
 
-        } while (moreGuests.equalsIgnoreCase("yes")); // if the String is anything besides yes then the loop stops
+            if (!guests.contains(name)) {
+                guests.add(name);
+            } else {
+                System.out.println("This guest is already in the list.");
+            }
 
-        System.out.println(guestList);
+            System.out.println("Do you want to enter another guest's name?");
+            answer = input.nextLine();
 
+        } while (answer.equalsIgnoreCase("yes"));
+
+        System.out.println("Guests' list: " + String.join(", ", guests));
     }
 }
