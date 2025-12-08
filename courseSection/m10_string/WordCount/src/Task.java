b79dcafd99bde10e688c2b33a6712cba2ae0b2ff
cherevach-glitch <cherevachandrei@gmail.com>
2025-12-08 20:59:09 +0200

    completed

--- courseSection/m10_string/WordCount/src/Task.java
+++ courseSection/m10_string/WordCount/src/Task.java
@@ -1,13 +1,16 @@
 
 public class Task {
-    public static void main(String[] args) {
-        System.out.println(wordCount("one two three"));
-        System.out.println(wordCount("foo bar"));
-    }
 
     public static int wordCount(String words) {
 
-        //Write your solution here
+        String[] arr = words.split(" ");
+        return arr.length;
     }
 
+    public static void main(String[] args) {
+
+        System.out.println(wordCount("one two three"));
+        System.out.println(wordCount("foo bar"));
+        System.out.println(wordCount("hello world java"));
+    }
 }
