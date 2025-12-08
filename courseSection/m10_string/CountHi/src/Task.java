254fe4989f5ebc3c10ec46e9d2c5304f594b5d39
cherevach-glitch <cherevachandrei@gmail.com>
2025-12-08 20:21:47 +0200

    completed

--- courseSection/m10_string/CountHi/src/Task.java
+++ courseSection/m10_string/CountHi/src/Task.java
@@ -1,14 +1,23 @@
 
 public class Task {
+
     public static void main(String[] args) {
+
         System.out.println(countHi("abc hi how"));
         System.out.println(countHi("hihi bye"));
+        System.out.println(countHi("apple"));
     }
 
     public static int countHi(String str) {
 
-        //Write your solution here
+        int count = 0;
 
+        for (int i = 0; i < str.length() - 1; i++) {
+            if (str.substring(i, i + 2).equals("hi")) {
+                count++;
+            }
+        }
+
+        return count;
     }
-
 }
