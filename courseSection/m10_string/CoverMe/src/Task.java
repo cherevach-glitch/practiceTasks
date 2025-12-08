673d55fe390d750a44fccdf204abb52421af97aa
cherevach-glitch <cherevachandrei@gmail.com>
2025-12-08 20:28:26 +0200

    completed

--- courseSection/m10_string/CoverMe/src/Task.java
+++ courseSection/m10_string/CoverMe/src/Task.java
@@ -1,13 +1,19 @@
-
 public class Task {
+
     public static void main(String[] args) {
+
         System.out.println(coverMe("java methods", "me"));
         System.out.println(coverMe("Certified Wooden Spoon", "o"));
+        System.out.println(coverMe("hello hello", "ello"));
+        System.out.println(coverMe("apples", "pears"));
     }
 
-    public static String coverMe(String main, String coverMe){
+    public static String coverMe(String main, String coverMe) {
 
-        //Write your solution here
+        if (!main.contains(coverMe)) {
+            return "[" + main + "]";
+        }
 
+        return main.replace(coverMe, "[" + coverMe + "]");
     }
 }
