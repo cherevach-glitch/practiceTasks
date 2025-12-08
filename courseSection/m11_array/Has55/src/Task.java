8214a979b6f24ef2375d3b4a0edb7c974ff6185b
cherevach-glitch <cherevachandrei@gmail.com>
2025-12-08 20:10:39 +0200

    completed Has55

--- courseSection/m11_array/Has55/src/Task.java
+++ courseSection/m11_array/Has55/src/Task.java
@@ -1,13 +1,28 @@
-
 public class Task {
-    public static void main(String[] args) {
-        System.out.println(has55(new int[]{1, 5, 5, 1, 1}));
 
+    public static void main(String[] args) {
+
+        int[] arr1 = {1, 5, 5, 1, 1};
+        System.out.println(has55(arr1));
+
+        int[] arr2 = {1, 5, 4, 1, 5};
+        System.out.println(has55(arr2));
+
+        int[] arr3 = {1, 4, 4, 1, 99};
+        System.out.println(has55(arr3));
+
+        int[] arr4 = {1, 5, 1, 2, 5, 5};
+        System.out.println(has55(arr4));
     }
 
     public static boolean has55(int[] nums) {
 
-        //Write your solution here
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] == 5 && nums[i + 1] == 5) {
+                return true;
+            }
+        }
 
+        return false;
     }
 }
