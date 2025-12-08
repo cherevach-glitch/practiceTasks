c4e3542b861e5158274bba4780804ab12977f1e4
cherevach-glitch <cherevachandrei@gmail.com>
2025-12-08 21:04:22 +0200

    completed

--- courseSection/m08_methods/HelloWorld/src/Task.java
+++ courseSection/m08_methods/HelloWorld/src/Task.java
@@ -1,20 +1,15 @@
 public class Task {
-    public static void main(String[] args) {
-
-        //Call methods here
-
-    }
 
     public static void hello() {
-
-       //Write your solution here
-
+        System.out.println("Hello");
     }
 
     public static void world() {
-
-        //Write your solution here
-
+        System.out.println("World!");
     }
 
+    public static void main(String[] args) {
+        hello();
+        world();
+    }
 }
