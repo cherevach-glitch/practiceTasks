70a74d5d14666041896d40b5cda2e412ce7c2242
cherevach-glitch <cherevachandrei@gmail.com>
2025-12-08 20:04:22 +0200

    completed AverageTemperature

--- courseSection/m11_array/AverageTemperature/src/Task.java
+++ courseSection/m11_array/AverageTemperature/src/Task.java
@@ -1,13 +1,20 @@
-
 public class Task {
-    public static void main(String[] args) {
-        System.out.println(averageTemperature(new double[]{80.0, 88.0, 88.0, 84.0, 82.0, 78.0, 60.0, 68.0}));
 
+    public static void main(String[] args) {
+
+        double[] temps = {80.0, 88.0, 88.0, 84.0, 82.0, 78.0, 60.0, 68.0};
+
+        System.out.println(averageTemperature(temps));
     }
 
-    public static double averageTemperature(double[] temps){
+    public static double averageTemperature(double[] temperatures) {
 
-        //Write your solution here
+        double sum = 0;
 
+        for (double temp : temperatures) {
+            sum += temp;
+        }
+
+        return sum / temperatures.length;
     }
 }
