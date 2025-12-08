5aaaffa0c8d11579face663a70eace370dd2c523
cherevach-glitch <cherevachandrei@gmail.com>
2025-12-08 21:29:42 +0200

    completed

--- courseSection/m06_selection_statements/GreetingHours/src/Task.java
+++ courseSection/m06_selection_statements/GreetingHours/src/Task.java
@@ -1,12 +1,17 @@
 public class Task {
+
     public static void main(String[] args) {
 
-        int hour = 1;
+        int hour = 7;
 
-        // ----Do not change below lines. needed for testing---
-        hour = args.length > 0 ? Integer.parseInt(args[0]) : hour;
-        //--------------------------------
-
-        //Write your solution here
+        if (hour >= 5 && hour <= 11) {
+            System.out.println("Good morning");
+        } else if (hour >= 12 && hour <= 17) {
+            System.out.println("Good afternoon");
+        } else if ((hour >= 18 && hour <= 23) || (hour >= 0 && hour < 5)) {
+            System.out.println("Good evening");
+        } else {
+            System.out.println("Invalid hour");
+        }
     }
 }
