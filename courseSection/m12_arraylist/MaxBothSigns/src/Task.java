202c89a559c5d052bd285b89a0aef8957181c3e8
cherevach-glitch <cherevachandrei@gmail.com>
2025-12-08 19:39:30 +0200

    completed MaxBothSigns

--- courseSection/m12_arraylist/MaxBothSigns/src/Task.java
+++ courseSection/m12_arraylist/MaxBothSigns/src/Task.java
@@ -1,18 +1,31 @@
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 public class Task {
+
     public static void main(String[] args) {
-        System.out.println(findMaxBoth(new ArrayList<>(List.of(20, 2, 5, -2, 8, -5))));
-        System.out.println(findMaxBoth(new ArrayList<>(List.of(-2, -23, -9, 2, 7, 20))));
-        System.out.println(findMaxBoth(new ArrayList<>(List.of(-2, -23, -9, 3, 7, 20))));
+
+        ArrayList<Integer> list1 = new ArrayList<>(Arrays.asList(20, 2, 5, -2, 8, -5));
+        ArrayList<Integer> list2 = new ArrayList<>(Arrays.asList(-2, -23, -9, 2, 7, 20));
+        ArrayList<Integer> list3 = new ArrayList<>(Arrays.asList(-2, -23, -9, 3, 7, 20));
+
+        System.out.println(findMaxBoth(list1)); // 5
+        System.out.println(findMaxBoth(list2)); // 2
+        System.out.println(findMaxBoth(list3)); // 0
     }
 
     public static int findMaxBoth(ArrayList<Integer> nums) {
 
-        //Write your solution here
+        int max = 0;
 
+        for (int each : nums) {
+            if (each > 0 && nums.contains(-each)) {
+                if (each > max) {
+                    max = each;
+                }
+            }
+        }
+
+        return max;
     }
-
 }
