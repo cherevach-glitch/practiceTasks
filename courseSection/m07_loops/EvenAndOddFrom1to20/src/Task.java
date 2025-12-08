b2c059e6123c0d62a6433183865f208b36382e73
cherevach-glitch <cherevachandrei@gmail.com>
2025-12-08 13:24:07 +0200

    completed EvenAndOddFrom1to20 task

--- courseSection/m07_loops/EvenAndOddFrom1to20/src/Task.java
+++ courseSection/m07_loops/EvenAndOddFrom1to20/src/Task.java
@@ -1,7 +1,18 @@
 public class Task {
     public static void main(String[] args) {
 
-        //Write your solution here
+        for (int i = 1; i <= 20; i++) {
+            if (i % 2 == 0) {
+                System.out.print(i + " ");
+            }
+        }
 
+        System.out.println();
+
+        for (int i = 1; i <= 20; i++) {
+            if (i % 2 != 0) {
+                System.out.print(i + " ");
+            }
+        }
     }
 }
