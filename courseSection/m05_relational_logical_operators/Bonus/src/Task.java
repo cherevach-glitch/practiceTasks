c1414a33f374f952f03d38fac58f624b3a1a76cc
cherevach-glitch <cherevachandrei@gmail.com>
2025-12-08 13:06:31 +0200

    completed  Bonus task

--- courseSection/m05_relational_logical_operators/Bonus/src/Task.java
+++ courseSection/m05_relational_logical_operators/Bonus/src/Task.java
@@ -2,14 +2,10 @@ public class Task {
     public static void main(String[] args) {
 
         int yearsWithCompany = 5;
-        int department = 5;
+        int department = 10;
 
-        // ----Do not change below lines. needed for testing---
-        yearsWithCompany = args.length > 0 ? Integer.parseInt(args[0]) : yearsWithCompany;
-        department = args.length > 0 ? Integer.parseInt(args[1]) : department;
-        //--------------------------------
+        boolean hasBonus = yearsWithCompany >= 5 && (department == 10 || department == 15);
 
-        //Write your solution here
-
+        System.out.println("hasBonus = " + hasBonus);
     }
 }
